e3ddc5518602690201b592ba98fb2360b82e2e10
twentyfive21 <tina.winnn@gmail.com>
2024-06-02 22:59:49 -0400

    update and delete added for controllers

--- springboot-dealership/src/main/java/com/pluralsight/springboot_dealership/controllers/VehiclesController.java
+++ springboot-dealership/src/main/java/com/pluralsight/springboot_dealership/controllers/VehiclesController.java
@@ -2,10 +2,7 @@ package com.pluralsight.springboot_dealership.controllers;
 
 import com.pluralsight.springboot_dealership.dao.VehicleDao;
 import com.pluralsight.springboot_dealership.models.Vehicle;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -36,8 +33,46 @@ public class VehiclesController {
         return vehicleDao.getVehiclesByMakeAndModel(make, model);
     }
 
+    // http://localhost:8080/vehicles?minYear=2018&maxYear=2020
     @RequestMapping(path="/vehicles", method = RequestMethod.GET, params = {"minYear","maxYear"})
     public ArrayList<Vehicle> getVehiclesByYear(@RequestParam("minYear") int minYear, @RequestParam("maxYear") int maxYear){
         return vehicleDao.getVehiclesByYear(minYear, maxYear);
     }
+
+    // http://localhost:8080/vehicles?color=white
+    @RequestMapping(path="/vehicles", method = RequestMethod.GET, params = {"color"})
+    public ArrayList<Vehicle> getVehiclesByColor(@RequestParam("color") String color){
+        return vehicleDao.getVehiclesByColor(color);
+    }
+
+    // http://localhost:8080/vehicles?minOdo=0&maxOdo=15000
+    @RequestMapping(path="/vehicles", method = RequestMethod.GET, params = {"minOdo","maxOdo"})
+    public ArrayList<Vehicle> getVehiclesByMileage(@RequestParam("minOdo") int minOdo, @RequestParam("maxOdo") int maxOdo){
+        return vehicleDao.getVehiclesByMileage(minOdo, maxOdo);
+    }
+
+    // http://localhost:8080/vehicles?type=sedan
+    @RequestMapping(path="/vehicles", method = RequestMethod.GET, params = {"type"})
+    public ArrayList<Vehicle> getVehiclesByType(@RequestParam("type") String type){
+        return vehicleDao.getVehiclesByType(type);
+    }
+
+    @RequestMapping(path="/vehicles", method = RequestMethod.POST)
+    public void addVehicleToDB(@RequestBody Vehicle vehicle){
+        vehicleDao.addVehicleToDB(vehicle);
+    }
+
+    @RequestMapping(path="/vehicles/{vin}", method = RequestMethod.DELETE)
+    public void removeVehicleToDB(@PathVariable int vin){
+        vehicleDao.removeVehicleToDB(vin);
+    }
+
+    @RequestMapping(path = "/vehicles/{vin}", method = RequestMethod.PUT)
+    public void updateVehicleToDB(@PathVariable int vin, @RequestBody Vehicle vehicle){
+        vehicleDao.updateVehicleToDB(vin,vehicle);
+    }
+
+
+
+
 }
--- springboot-dealership/src/main/java/com/pluralsight/springboot_dealership/dao/JdbcVehicleDao.java
+++ springboot-dealership/src/main/java/com/pluralsight/springboot_dealership/dao/JdbcVehicleDao.java
@@ -158,17 +158,141 @@ public class JdbcVehicleDao implements VehicleDao{
 
     @Override
     public ArrayList<Vehicle> getVehiclesByColor(String userColor) {
-        return null;
+        ArrayList<Vehicle> allVehiclesFromDB = new ArrayList<>();
+        try{
+            try(
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Vehicles WHERE Color LIKE ?;")
+            ){
+                // no need to set string since passed already with no parameters
+                preparedStatement.setString(1, "%" + userColor + "%");
+
+                try(
+                        ResultSet resultSet = preparedStatement.executeQuery();
+                ){
+                    if (resultSet.next()) {
+                        // if there are, you are already sitting on the first one so
+                        // switch your loop to using a do/while
+                        do {
+                            int vin = resultSet.getInt("Vin");
+                            int vehicleYear = resultSet.getInt("VehicleYear");
+                            String make = resultSet.getString("Make");
+                            String model = resultSet.getString("Model");
+                            String vehicleType = resultSet.getString("VehicleType");
+                            String color = resultSet.getString("Color");
+                            int odometer = resultSet.getInt("Odometer");
+                            double price = resultSet.getDouble("Price");
+                            // Display the customer information
+                            Vehicle vehicle = new Vehicle(vin,vehicleYear,make,model,vehicleType,color,odometer,price);
+                            // add to array list
+                            allVehiclesFromDB.add(vehicle);
+                        } while (resultSet.next());
+                    }
+                    else {
+                        System.out.println("Error getting vehicles from database");
+                    }
+                }
+            }
+
+        }catch (SQLException e){
+            // This will catch all SQLExceptions occurring in the
+            // block including those in nested try statements
+            e.printStackTrace();
+        }
+        return allVehiclesFromDB;
     }
 
     @Override
     public ArrayList<Vehicle> getVehiclesByMileage(double minMiles, double maxMiles) {
-        return null;
+        ArrayList<Vehicle> allVehiclesFromDB = new ArrayList<>();
+        try{
+            try(
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Vehicles WHERE Odometer >= ? AND Odometer <= ? ORDER BY Odometer;")
+            ){
+                // no need to set string since passed already with no parameters
+                preparedStatement.setDouble(1, minMiles);
+                preparedStatement.setDouble(2, maxMiles);
+
+                try(
+                        ResultSet resultSet = preparedStatement.executeQuery();
+                ){
+                    if (resultSet.next()) {
+                        // if there are, you are already sitting on the first one so
+                        // switch your loop to using a do/while
+                        do {
+                            int vin = resultSet.getInt("Vin");
+                            int vehicleYear = resultSet.getInt("VehicleYear");
+                            String make = resultSet.getString("Make");
+                            String model = resultSet.getString("Model");
+                            String vehicleType = resultSet.getString("VehicleType");
+                            String color = resultSet.getString("Color");
+                            int odometer = resultSet.getInt("Odometer");
+                            double price = resultSet.getDouble("Price");
+                            // Display the customer information
+                            Vehicle vehicle = new Vehicle(vin,vehicleYear,make,model,vehicleType,color,odometer,price);
+                            // add to array list
+                            allVehiclesFromDB.add(vehicle);
+                        } while (resultSet.next());
+                    }
+                    else {
+                        System.out.println("Error getting vehicles from database");
+                    }
+                }
+            }
+
+        }catch (SQLException e){
+            // This will catch all SQLExceptions occurring in the
+            // block including those in nested try statements
+            e.printStackTrace();
+        }
+        return allVehiclesFromDB;
     }
 
     @Override
     public ArrayList<Vehicle> getVehiclesByType(String userType) {
-        return null;
+        ArrayList<Vehicle> allVehiclesFromDB = new ArrayList<>();
+        try{
+            try(
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Vehicles WHERE VehicleType LIKE ?;")
+            ){
+                // no need to set string since passed already with no parameters
+                preparedStatement.setString(1, "%" + userType + "%");
+
+                try(
+                        ResultSet resultSet = preparedStatement.executeQuery();
+                ){
+                    if (resultSet.next()) {
+                        // if there are, you are already sitting on the first one so
+                        // switch your loop to using a do/while
+                        do {
+                            int vin = resultSet.getInt("Vin");
+                            int vehicleYear = resultSet.getInt("VehicleYear");
+                            String make = resultSet.getString("Make");
+                            String model = resultSet.getString("Model");
+                            String vehicleType = resultSet.getString("VehicleType");
+                            String color = resultSet.getString("Color");
+                            int odometer = resultSet.getInt("Odometer");
+                            double price = resultSet.getDouble("Price");
+                            // Display the customer information
+                            Vehicle vehicle = new Vehicle(vin,vehicleYear,make,model,vehicleType,color,odometer,price);
+                            // add to array list
+                            allVehiclesFromDB.add(vehicle);
+                        } while (resultSet.next());
+                    }
+                    else {
+                        System.out.println("Error getting vehicles from database");
+                    }
+                }
+            }
+
+        }catch (SQLException e){
+            // This will catch all SQLExceptions occurring in the
+            // block including those in nested try statements
+            e.printStackTrace();
+        }
+        return allVehiclesFromDB;
     }
 
     @Override
@@ -222,16 +346,86 @@ public class JdbcVehicleDao implements VehicleDao{
 
     @Override
     public void addVehicleToDB(Vehicle vehicle) {
+        try {
+            try (
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement preparedStatement1 = connection.prepareStatement(
+                            "INSERT INTO Vehicles (Vin, VehicleYear, Make, Model, VehicleType, Color, Odometer, Price, Sold) " +
+                                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, FALSE)");
+                    PreparedStatement preparedStatement2 = connection.prepareStatement(
+                            "INSERT INTO Inventory (Dealership_id, Vin) VALUES (1, ?)")
+            ) {
+                // Insert into Vehicles table
+                preparedStatement1.setInt(1, vehicle.getVin());
+                preparedStatement1.setInt(2, vehicle.getYear());
+                preparedStatement1.setString(3, vehicle.getMake());
+                preparedStatement1.setString(4, vehicle.getModel());
+                preparedStatement1.setString(5, vehicle.getVehicleType());
+                preparedStatement1.setString(6, vehicle.getColor());
+                preparedStatement1.setInt(7, vehicle.getOdometer());
+                preparedStatement1.setDouble(8, vehicle.getPrice());
+                preparedStatement1.executeUpdate();
 
+                // Insert into Inventory table
+                preparedStatement2.setInt(1, vehicle.getVin());
+                preparedStatement2.executeUpdate();
+
+                System.out.println("Vehicle added to Vehicles and Inventory tables successfully");
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void updateVehicleToDB(int vin, Vehicle vehicle) {
+        try{
+            try(
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement preparedStatement =
+                            connection.prepareStatement("UPDATE Vehicles SET Odometer = ?, Price = ? WHERE Vin = ?;");
+            ){
+                preparedStatement.setInt(1,vehicle.getOdometer());
+                preparedStatement.setDouble(2,vehicle.getPrice());
+                preparedStatement.setInt(3,vin);
+                preparedStatement.executeUpdate();
 
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Error updating category");
+        }
     }
 
     @Override
     public void removeVehicleToDB(int userVin) {
+        try {
+            try (
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement preparedStatement1 = connection.prepareStatement(
+                            "DELETE FROM LeaseContracts WHERE Vin = ?");
+                    PreparedStatement preparedStatement2 = connection.prepareStatement(
+                            "DELETE FROM SalesContracts WHERE Vin = ?");
+                    PreparedStatement preparedStatement3 = connection.prepareStatement(
+                            "DELETE FROM Inventory WHERE Vin = ?");
+                    PreparedStatement preparedStatement4 = connection.prepareStatement(
+                            "DELETE FROM Vehicles WHERE Vin = ?");
+            ) {
+                preparedStatement1.setInt(1, userVin);
+                preparedStatement1.executeUpdate();
+                preparedStatement2.setInt(1, userVin);
+                preparedStatement2.executeUpdate();
+                preparedStatement3.setInt(1, userVin);
+                preparedStatement3.executeUpdate();
+                preparedStatement4.setInt(1, userVin);
+                preparedStatement4.executeUpdate();
 
+                System.out.println("Vehicle deleted from Vehicles and Inventory tables successfully");
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
