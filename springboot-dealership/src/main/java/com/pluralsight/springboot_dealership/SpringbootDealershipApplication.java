b4aa47ec6fc57b63b9c67aaa9a61dd4c15f8aecc
twentyfive21 <tina.winnn@gmail.com>
2024-06-02 21:35:37 -0400

    setting up springboot

---
+++ springboot-dealership/src/main/java/com/pluralsight/springboot_dealership/SpringbootDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springboot_dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDealershipApplication.class, args);
+	}
+
+}
---
+++ springboot-dealership/src/test/java/com/pluralsight/springboot_dealership/SpringbootDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springboot_dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
